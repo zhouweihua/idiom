7b113f12ae4e837dbbcb162f1321d124432234db
huainanzi <huainanzi@wacai.com>
2020-10-18 09:51:05 +0800

    6-编辑页面

--- src/page/idiomEdit/index.js
+++ src/page/idiomEdit/index.js
@@ -38,29 +38,40 @@ export default class IdiomEdit extends React.Component {
           </div>
         </div>
 
-        <div className="idiomDeatailsCon">
-          <div className="idiomDeatails">
-            <div className="idiomDeatailItem">
-              <div className="idiomSymbols">
-                <div className="textLeft">Chinese phonetic symbols：</div>
-                <div className="textRight">
-                  <p>zhǐ   xǔ   zhōu   guān   fàng   huǒ，bù   xǔ   bǎi   xìng   diǎn   dēng</p>
-                  <p>只   许  州  官   放  火，不   许  百  姓  点  灯</p>
-                </div>
+        <div className="idiomEditCon">
+          <div className="idiomEdit">
+            <div className="editMainDes">More Appropriate English Interpretation</div>
+            <div className="editSubDes">If you have a more appropriate English interpretation, you can correct it and it will be uploaded to the website after being approved</div>
+            
+            <div className="editItem">
+              <div className="editMainTitle">
+                Chinese phonetic symbols
               </div>
-              <div className="idiomExplanation">
-                <div className="textLeft">Chinese explanation：</div>
-                <div className="textRight">比泰山还要安稳，形容非常平安稳固，不可动摇，比泰山还要安稳，形容非常
-                平安稳固，不可动摇</div>
+              <input className="editCont" />
+            </div>
+            <div className="editItem">
+              <div className="editMainTitle">
+                Chinese explanation
               </div>
-              <div className="idiomInterpret">
-                <div className="textLeft">English interpretation：</div>
-                <div className="textRight">Even more stable than Mount Tai, it describes a very safe and stable, unshakable</div>
+              <input className="editCont" />
+            </div>
+            <div className="editItem">
+              <div className="editMainTitle">
+                English interpretation
               </div>
-              <div className="idiomEdit">Edit</div>
+              <input className="editCont" />
             </div>
           </div>
         </div>
+
+        <div className="idiomSubmitCon">
+          <div className="idiomSubmit">
+            <div className="submitButton">
+              SUBMIT
+            </div>
+          </div>
+        </div>
+
       </div>
     )
   }
